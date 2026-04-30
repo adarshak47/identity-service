dc279f2e1c58df402e95e60325e4c0e474a61c7c
Adarsh Kubihal <adarshkubihal@gmail.com>
2026-04-30 15:19:23 +0530

    api-standard-response: feat - Standardize API responses with ApiResponse wrapper.

--- src/main/java/com/adarsh/identity_service/auth/controller/AuthController.java
+++ src/main/java/com/adarsh/identity_service/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.adarsh.identity_service.auth.controller;
 import com.adarsh.identity_service.auth.dto.*;
 import com.adarsh.identity_service.auth.service.AuthenticationService;
 
+import com.adarsh.identity_service.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 import jakarta.validation.Valid;
@@ -19,31 +20,23 @@ public class AuthController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public RegisterResponse register(
-        @Valid @RequestBody RegisterRequest request
-    ) {
-        return authenticationService.registerUser(request);
+    public ApiResponse<RegisterResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ApiResponse.success(authenticationService.registerUser(request));
     }
 
     @PostMapping("/login")
-    public LoginResponse login(
-        @Valid @RequestBody LoginRequest request
-    ){
-        return authenticationService.login(request);
+    public ApiResponse<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ApiResponse.success(authenticationService.login(request));
     }
 
     @PostMapping("/refresh")
-    public LoginResponse refresh(
-        @RequestBody @Valid RefreshRequest request
-    ){
-        return authenticationService.refreshToken(request);
+    public ApiResponse<LoginResponse> refresh(@Valid @RequestBody RefreshRequest request) {
+        return ApiResponse.success(authenticationService.refreshToken(request));
     }
 
     @PostMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void logout(
-        @Valid @RequestBody LogoutRequest request
-    ){
+    public void logout(@Valid @RequestBody LogoutRequest request){
         authenticationService.logout(request);
     }
 }
